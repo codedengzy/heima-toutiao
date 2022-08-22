dbebe7c86bc323ac07db6a78cc2a5ecf260c1847
codedzy <codedzy@qq.com>
2022-08-22 21:58:49 +0800

    更新配置

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,15 @@
 -->
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
     <h1>移动端头条</h1>
     <div>
       <i class="iconfont icon-shoucang"></i>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
     </div>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/styles/index.less'
+// 引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
 
+Vue.use(Vant)
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
